bfc47f456020645ee25b2804a0a3cfc50de0791b
zogreptile <zogreptile@gmail.com>
2020-10-11 21:42:15 +0700

    refactor User page

--- src/pages/user/index.js
+++ src/pages/user/index.js
@@ -1,1 +1,24 @@
-export { default } from './user';
+import { connect } from "react-redux";
+
+import { getUserinfo } from 'src/actions/user-info';
+import { getRepos } from 'src/actions/repos';
+import { loadMoreRepos } from 'src/actions/load-more-repos';
+import { getRepoInfo } from 'src/actions/repo-info';
+
+import UserPage from './user';
+
+const mapStateToProps = state => ({
+  repos: state.repos.items,
+  filter: state.filter,
+  sort: state.sort,
+  nextPageUrl: state.repos.pagination.next.url,
+});
+
+const mapDispatchToProps = {
+  getUserinfo,
+  getRepos,
+  getRepoInfo,
+  loadMoreRepos,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(UserPage);
--- src/pages/user/user.jsx
+++ src/pages/user/user.jsx
@@ -1,19 +1,21 @@
-import { Grid } from 'semantic-ui-react';
-import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
+import {
+  Grid,
+  Card,
+} from 'semantic-ui-react';
+import flow from "lodash.flow";
+import moment from 'moment';
 
 import MainLayout from 'src/layouts/main';
 
 import UserInfo from 'src/components/user-info';
-import Repos from 'src/components/repos';
+import RepoCard from 'src/components/repo-card';
 import LoadMoreBtn from 'src/components/load-more-btn';
 import RepoModal from 'src/components/repo-modal';
 import ReposFilter from 'src/components/repos-filter';
 import ReposSort from 'src/components/repos-sort';
 
-import { getUserinfo } from 'src/actions/user-info';
-import { getRepos } from 'src/actions/repos';
-import { loadMoreRepos } from 'src/actions/load-more-repos';
+import filterRepos from 'src/utlis/filter-repos';
+import sortRepos from 'src/utlis/sort-repos';
 
 const propTypes = {
   match: PropTypes.shape({
@@ -23,19 +25,23 @@ const propTypes = {
   }),
   getUserinfo: PropTypes.func,
   getRepos: PropTypes.func,
+  getRepoInfo: PropTypes.func,
   nextPageUrl: PropTypes.string,
   loadMoreRepos: PropTypes.func,
+  repos: PropTypes.arrayOf(PropTypes.shape({
+    owner: PropTypes.shape({
+      login: PropTypes.string,
+    }),
+    name: PropTypes.string,
+    description: PropTypes.string,
+    updated_at: PropTypes.string,
+    language: PropTypes.string,
+    fork: PropTypes.bool,
+    stargazers_count: PropTypes.number,
+  })),
+  filter: PropTypes.shape({}),
+  sort: PropTypes.shape({}),
 }
-
-const mapStateToProps = state => ({
-  nextPageUrl: state.repos.pagination.next.url,
-});
-
-const mapDispatchToProps = {
-  getUserinfo,
-  getRepos,
-  loadMoreRepos,
-};
 
 class UserPage extends React.Component {
   componentDidMount() {
@@ -45,24 +51,71 @@ class UserPage extends React.Component {
     getRepos(match.params.id);
   }
 
+  loadMoreRepos = () => {
+    this.props.loadMoreRepos(this.props.nextPageUrl);
+  }
+
+  processRepos = (repos) => {
+    const { filter, sort } = this.props;
+
+    const repositories = repos.map((repo) => ({
+      ...repo,
+      lastUpdatedDate: moment(repo.updated_at).format('DD.MM.YYYY'),
+    }));
+
+    const processedRepos = flow(
+      filterRepos(filter),
+      sortRepos(sort),
+    )(repositories);
+
+    return processedRepos;
+  }
+
   render() {
     const { nextPageUrl, loadMoreRepos } = this.props;
 
     return (
       <MainLayout>
         <Grid>
-          <Grid.Column mobile={16} computer={4}>
+          <Grid.Column
+            mobile={16}
+            computer={4}
+          >
             <UserInfo />
             <ReposFilter />
             <ReposSort />
           </Grid.Column>
 
-          <Grid.Column mobile={16} computer={12}>
-            <Repos />
-            <LoadMoreBtn
-              url={nextPageUrl}
-              action={loadMoreRepos}
-            />
+          <Grid.Column
+            mobile={16}
+            computer={12}
+          >
+            {!this.props.repos.length
+              ? <h1>No repositories</h1>
+              : (
+                <Card.Group itemsPerRow='3' doubling>
+                  {this.processRepos(this.props.repos).map((repo) => (
+                    <RepoCard
+                      key={repo.id}
+                      ownerUsername={repo.owner.login}
+                      title={repo.name}
+                      description={repo.description}
+                      lastUpdatedDate={repo.lastUpdatedDate}
+                      language={repo.language}
+                      isFork={repo.fork}
+                      stargazersCount={repo.stargazers_count}
+                      getRepoInfo={this.props.getRepoInfo}
+                    />
+                  ))}
+                </Card.Group>
+              )
+            }
+
+            {!!this.props.nextPageUrl && (
+              <LoadMoreBtn
+                onClick={this.loadMoreRepos}
+              />
+            )}
           </Grid.Column>
 
           <RepoModal />
@@ -74,4 +127,4 @@ class UserPage extends React.Component {
 
 UserPage.propTypes = propTypes;
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(UserPage));
+export default UserPage;
