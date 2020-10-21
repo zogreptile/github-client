493ab01f3e0fba051621d738a4698c40546073ec
zogreptile <zogreptile@gmail.com>
2020-10-22 01:03:46 +0700

    mock user and users requests

--- src/mocks.js
+++ src/mocks.js
@@ -1,4 +1,31 @@
 const axios = require("axios");
 const MockAdapter = require("axios-mock-adapter");
 
+const users = require('src/mocks/users');
+const user = require('src/mocks/user');
+
 const mock = new MockAdapter(axios);
+
+mock
+  .onGet(/\/search\/users(.*)/)
+  .reply(() => {
+    console.warn('asdasdas')
+    const headers = {
+      link: '<https://api.github.com/search/users?q=zog&page=2>; rel="next", <https://api.github.com/search/users?q=zog&page=22>; rel="last"',
+    };
+
+    return [
+      200,
+      users,
+      headers,
+    ];
+  });
+
+mock
+  .onGet(/\/users\/(.*)/)
+  .reply(() => {
+    return [
+      200,
+      user,
+    ];
+  });
