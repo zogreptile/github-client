1dee6deccf6736f9dde59a6001c941d1295a6c80
zogreptile <zogreptile@gmail.com>
2020-11-03 00:09:45 +0700

    mock users/repos request

--- src/mocks.js
+++ src/mocks.js
@@ -3,6 +3,7 @@ const MockAdapter = require("axios-mock-adapter");
 
 const users = require('src/mocks/users');
 const user = require('src/mocks/user');
+const repos = require('src/mocks/repos');
 
 const mock = new MockAdapter(axios);
 
@@ -16,6 +17,20 @@ mock
     return [
       200,
       users,
+      headers,
+    ];
+  });
+
+mock
+  .onGet(/\/users\/(.*)\/repos/)
+  .reply((config) => {
+    const headers = {
+      link: '<https://api.github.com/user/174688/repos?page=2>; rel="next", <https://api.github.com/user/174688/repos?page=3>; rel="last"',
+    };
+
+    return [
+      200,
+      repos,
       headers,
     ];
   });
